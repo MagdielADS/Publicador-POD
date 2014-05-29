038250b0ea232467ce02268adde37a9cbbc67f71
magdiel-bruno <magdiel-bruno@magdiel-pc>
2014-05-29 13:54:15 -0300

    Publicador, o início!

---
+++ src/main/java/br/edu/ifpb/pod/connection/ConnectionDataBase.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifpb.pod.connection;
+
+/**
+ *
+ * @author magdiel-bruno
+ */
+public class ConnectionDataBase {
+    
+}
